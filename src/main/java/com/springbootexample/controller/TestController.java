6361c294bb1174657c9acfaac0b862db450f6478
Bala09555 <balanagireddy1997@gmail.com>
2024-02-20 23:29:23 +0530

    LOGS

--- src/main/java/com/springbootexample/controller/TestController.java
+++ src/main/java/com/springbootexample/controller/TestController.java
@@ -2,18 +2,21 @@ package com.springbootexample.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-	@GetMapping
+	private Logger logger=LoggerFactory.getLogger(TestController.class);
+	@GetMapping ("/api")
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
 		object.put("name", "Rntci");
 		object.put("email", "rntbci@gmail.com");
+		logger.info("Welcome to Azure");
 		return object;
 	}
 
